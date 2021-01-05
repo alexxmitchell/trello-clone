c7742d8ba9bf39273a130879dc97d467c3f5ebd2
Alexx Mitchell <alexxmitchell25@gmail.com>
2021-01-04 19:33:53 -0500

    set up apolloclient, can login on client

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,11 +1,25 @@
 import React, {useState } from 'react'
+import { useMutation, gql } from '@apollo/client';
 import {Link} from 'react-router-dom';
 import Logo from '../Logo/Logo'
 
+const LOGIN_USER = gql`
+  mutation loginUser($password: String, $email: String) {
+    loginUser(password: $password, email: $email) {
+      id
+    }
+  }
+`
 function Login() {
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [loginUser, {data}] = useMutation(LOGIN_USER);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    loginUser({variables: {password: password, email: username}});
+  }
   return (
     <div>
       <header>
@@ -13,7 +27,8 @@ function Login() {
       </header>
       <div className="login-container">
         <h2>Log in to Trello</h2>
-        <form onSubmit=''>
+
+        <form onSubmit={onSubmit}>
           <input type="text" name={username} onChange={e => setUsername(e.target.value)} />
           <input type="password" name={password} onChange={e => setPassword(e.target.value)}/>
           <input type="submit" value="Log in" />
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'http://localhost:4000/graphql'
+})
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
